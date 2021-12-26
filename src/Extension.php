26f76a6434ebd09a90498a0bba678015e5b7ef85
Abdellah Ramadan <ramadanabdel24@gmail.com>
2021-12-26 12:05:51 +0100

    Initial setup and commit

---
+++ src/Extension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AbdellahRamadan\BoltContentAuthor;
+
+use Bolt\Extension\BaseExtension;
+
+class Extension extends BaseExtension
+{
+    public function getName(): string
+    {
+        return 'Bolt Content Author';
+    }
+}
